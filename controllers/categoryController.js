830a3235dace817c44e4bb6d1bf9c5cb40eaa638
Morgan Douglas <morgando@umich.edu>
2019-07-25 12:24:51 -0700

    Makes search routing/control methods work
    Items for a selected category will now show up on
    display.

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,12 +1,55 @@
 let Category = require('../models/category')
+let Item = require('../models/item')
+let async = require('async')
 
-exports.category_list = function(req, res, next) {
-    Category.find()
-    .sort([['name', 'ascending']])
-    .exec(function (err, list_categories) {
-        if(err) { return next(err); }
 
-        res.render('index', {categories: list_categories});
-    });
+exports.search_category = function(req, res) {
+    async.series({
+            categories: function(callback) {
+                console.log('inside category count')
+                Category.find({}, 'name id')
+                .exec(function (err, list_categories) {
+                    console.log('done with category count')
+                    return callback(err, list_categories);
+                });
+
+            },
+            items: function(callback) {
+                console.log('inside item count')
+                console.log(req.query);
+                console.log(req.query.category);
+                if(req.query.category) {
+                    Item.find({category: req.query.category}, 'name')
+                    .exec(function (err, list_items) {
+
+                        return callback(err, list_items);
+                    }); 
+                } else {
+                    console.log('im here')
+                    return callback([]);
+                }                
+            }
+        }, 
+        //callback
+        function(err, results) {
+            console.log('hellloooo')
+            if(err) {
+                console.log('there is error')
+                console.log(err);
+            }
+            console.log('rendering now!')
+            console.log(results);
+            console.log(results.items == true);
+            res.render('index', { data: results });
+        }
+    );
+
+    console.log('-_-')
 };
 
+// exports.search_results = function(req, res) {
+//     Item.find({category: req.body.category}, 'name')
+//     .exec(function (err, list_items) {
+
+//     })
+// }
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ var router = express.Router();
 let category_controller = require('../controllers/categoryController');
 
 /* GET home page. */
-router.get('/', category_controller.category_list);
+router.get('/', category_controller.search_category);
+
 
 module.exports = router;
